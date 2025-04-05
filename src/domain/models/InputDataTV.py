2d6873f19b5f498664c40e55b98ac4d2f200f78c
DanielCondia <daniel.condia@gmail.com>
2025-04-05 00:30:53 -0500

    TF-1: ♻️ Se hace limpieza del objeto de entreada de alerta de trading view

--- src/domain/models/InputDataTV.py
+++ src/domain/models/InputDataTV.py
@@ -4,7 +4,4 @@ from pydantic import BaseModel
 class InputDataTV(BaseModel):
     symbol: str  # Símbolo de la moneda
     side: str  # Cambiado de "buy" a "sell"
-    # tradeSide: str  # Cambiado de "close" a "open"
-    percentage: bool  # Porcentaje para abrir la orden
     size: float  # Porcentaje para abrir la orden si es porcentaje, el tamaño de la orden si es tamaño
-    exchange: str  # Define el exchange en donde se va a ejecutar la operación
