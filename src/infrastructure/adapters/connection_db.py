8457d8bac23e41dfe09720407c86b49e786f79bc
DanielCondia <daniel.condia@gmail.com>
2025-04-10 22:07:56 -0500

    merge branch TF-1 into develop

--- src/infrastructure/adapters/connection_db.py
+++ src/infrastructure/adapters/connection_db.py
@@ -1,7 +1,9 @@
+from typing import Dict
+
 from src.domain.interfaces.connection_service_database import ConnectionServiceDatabase
 
 
 class ConnectionDB(ConnectionServiceDatabase):
 
-    def get_array_api_keys(self):
+    def get_array_api_keys(self) -> Dict[str, str]:
         pass
