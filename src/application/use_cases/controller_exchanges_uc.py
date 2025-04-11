1a82c6e33ec9d38758fdb19342fc0a7926db6338
DanielCondia <daniel.condia@gmail.com>
2025-04-10 21:28:40 -0500

    TF-1: Cambio de paquete connection dtabase y se convierte en clase abs

--- src/application/use_cases/controller_exchanges_uc.py
+++ src/application/use_cases/controller_exchanges_uc.py
@@ -4,7 +4,7 @@ from src.application.use_cases.position_bitget_uc import PositionBitgetUC
 from src.domain.models import InputDataTV
 from src.domain.models.response import Response
 from src.infrastructure.adapters.bitget_auth import BitgetAuth
-from src.infrastructure.adapters.connection_service_database import ConnectionServiceDatabase
+from src.domain.interfaces.connection_service_database import ConnectionServiceDatabase
 
 
 class ControllerExchangesUC:
